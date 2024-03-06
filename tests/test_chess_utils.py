908e9d00d37a0bdd5c2f677af0865fe2264cb758
aadjones <aaron.demby.jones@gmail.com>
2024-03-05 19:37:10 -0800

    Parametrize tests

--- tests/test_chess_utils.py
+++ tests/test_chess_utils.py
@@ -1,3 +1,4 @@
+import pytest
 from logic.chess_utils import (
     find_deviation,
     read_pgn,
@@ -9,87 +10,46 @@ from logic.deviation_result import DeviationResult
 PGN_PATH = "pgns/"
 
 
-def test_find_deviation_white():
-    ref_game = read_pgn(PGN_PATH + "carlsen-nakamura-2020.pgn")
-    my_game = read_pgn(PGN_PATH + "harpseal-rayrey784.pgn")
-
-    assert find_deviation(ref_game, my_game, "HarpSeal") == DeviationResult(
-        6, "Qc2", "Bf4", "White"
-    )
-
-
-def test_find_deviation_black():
-    ref_game = read_pgn(PGN_PATH + "ref-acc-dragon-1.pgn")
-    my_game = read_pgn(PGN_PATH + "acc-dragon-test-1.pgn")
-
-    assert find_deviation(ref_game, my_game, "Goumas, G.") == DeviationResult(
-        11, "b6", "a6", "Black"
-    )
-
-
-def test_find_deviation_black_a5():
-    ref_game = read_pgn(PGN_PATH + "ref-acc-dragon-1.pgn")
-    my_game = read_pgn(PGN_PATH + "a5-should-be-Re8-jrjrjr4.pgn")
-
-    assert find_deviation(ref_game, my_game, "Jrjrjr4") == DeviationResult(
-        8, "a5", "Re8", "Black"
-    )
+@pytest.mark.parametrize(
+    "ref_pgn_path, my_pgn_path, player, expected",
+    [
+        ("carlsen-nakamura-2020.pgn", "harpseal-rayrey784.pgn", "HarpSeal", DeviationResult(6, "Qc2", "Bf4", "White")),
+        ("ref-acc-dragon-1.pgn", "acc-dragon-test-1.pgn", "Goumas, G.", DeviationResult(11, "b6", "a6", "Black")),
+        ("ref-acc-dragon-1.pgn", "a5-should-be-Re8-jrjrjr4.pgn", "Jrjrjr4", DeviationResult(8, "a5", "Re8", "Black")),
+        ("carlsen-nakamura-2018.pgn", "carlsen-nakamura-2018.pgn", "Carlsen, M.", None),
+        ("acc-dragon-test-1.pgn", "carlsen-nakamura-2018.pgn", "Carlsen, M.", None),
+        ("acc-dragon-test-1.pgn", "opponent-deviates-first.pgn", "Jrjrjr4", None),
+        ("acc-dragon-test-1.pgn", "jrjrjr4-last-game-mar-2.pgn", "Jrjrjr4", DeviationResult(1, "d4", "e4", "White")),
+    ]
+)
+def test_find_deviation(ref_pgn_path, my_pgn_path, player, expected):
+    PGN_PATH = "pgns/"
+    ref_game = read_pgn(PGN_PATH + ref_pgn_path)
+    my_game = read_pgn(PGN_PATH + my_pgn_path)
+    assert find_deviation(ref_game, my_game, player) == expected
 
 
-def test_find_deviation_when_equal():
-    ref_game = read_pgn(PGN_PATH + "carlsen-nakamura-2018.pgn")
+@pytest.mark.parametrize(
+    "my_pgn_path, player, expected",
+    [
+        ("harpseal-rayrey784.pgn", "HarpSeal", "White"),
+        ("jrjrjr4-last-game.pgn", "Jrjrjr4", "Black")
+    ]
+)
+def test_get_player_color(my_pgn_path, player, expected):
+    PGN_PATH = "pgns/"
+    my_game = read_pgn(PGN_PATH + my_pgn_path)
+    assert get_player_color(my_game, player) == expected
 
-    assert find_deviation(ref_game, ref_game, "Carlsen, M.") is None
+@pytest.mark.parametrize(
+    "ref_study_url, my_pgn_path, player, expected",
+    [
+        ("https://lichess.org/study/bve0Qw48/VgqoWwgW", "a5-should-be-Re8-jrjrjr4.pgn", "Jrjrjr4", DeviationResult(8, "a5", "Re8", "Black")),
+        ("https://lichess.org/study/bve0Qw48/VgqoWwgW", "played-e6-instead-of-Nd4.pgn", "Jrjrjr4", DeviationResult(5, "e6", "Nd4", "Black"))
+    ]
+)
+def test_find_deviation_in_entire_study(ref_study_url, my_pgn_path, player, expected):
+    PGN_PATH = "pgns/"
+    my_game = read_pgn(PGN_PATH + my_pgn_path)
+    assert find_deviation_in_entire_study(ref_study_url, my_game, player) == expected
 
-
-def test_find_deviation_when_wrong_color():
-    ref_game = read_pgn(PGN_PATH + "acc-dragon-test-1.pgn")
-    my_game = read_pgn(PGN_PATH + "carlsen-nakamura-2018.pgn")
-
-    assert find_deviation(ref_game, my_game, "Carlsen, M.") is None
-
-
-def test_opponent_deviates_first():
-    ref_game = read_pgn(PGN_PATH + "acc-dragon-test-1.pgn")
-    my_game = read_pgn(PGN_PATH + "opponent-deviates-first.pgn")
-
-    assert find_deviation(ref_game, my_game, "Jrjrjr4") is None
-
-
-def test_wrong_color_test_2():
-    ref_game = read_pgn(PGN_PATH + "acc-dragon-test-1.pgn")
-    my_game = read_pgn(PGN_PATH + "jrjrjr4-last-game-mar-2.pgn")
-
-    assert find_deviation(ref_game, my_game, "Jrjrjr4") == DeviationResult(
-        1, "d4", "e4", "White"
-    )
-
-
-def test_get_player_color_white():
-    my_game = read_pgn(PGN_PATH + "harpseal-rayrey784.pgn")
-
-    assert get_player_color(my_game, "HarpSeal") == "White"
-
-
-def test_get_player_color_black():
-    my_game = read_pgn(PGN_PATH + "jrjrjr4-last-game.pgn")
-
-    assert get_player_color(my_game, "Jrjrjr4") == "Black"
-
-
-def test_find_deviation_in_entire_study_ch_1():
-    ref_study_url = "https://lichess.org/study/bve0Qw48/VgqoWwgW"
-    my_game = read_pgn(PGN_PATH + "a5-should-be-Re8-jrjrjr4.pgn")
-
-    assert find_deviation_in_entire_study(
-        ref_study_url, my_game, "Jrjrjr4"
-    ) == DeviationResult(8, "a5", "Re8", "Black")
-
-
-def test_find_deviation_in_entire_study_later_chapter():
-    ref_study_url = "https://lichess.org/study/bve0Qw48/VgqoWwgW"
-    my_game = read_pgn(PGN_PATH + "played-e6-instead-of-Nd4.pgn")
-
-    assert find_deviation_in_entire_study(
-        ref_study_url, my_game, "Jrjrjr4"
-    ) == DeviationResult(5, "e6", "Nd4", "Black")
--- tests/test_lichess_api.py
+++ tests/test_lichess_api.py
@@ -1,22 +1,19 @@
+import pytest
 from logic.lichess_api import (
     extract_study_id_from_url,
     get_study_chapters_count,
 )
 
-
-def test_extract_study_id_from_url_base():
-    url = "https://lichess.org/study/RKEBYTWL"
-    assert extract_study_id_from_url(url) == "RKEBYTWL"
-
-
-def test_extract_study_id_from_url_using_chapter():
-    url = "https://lichess.org/study/RKEBYTWL/muR4Kgyc"
-    assert extract_study_id_from_url(url) == "RKEBYTWL"
-
-
-def test_extract_study_id_from_url_using_chapter_2():
-    url = "https://lichess.org/study/bve0Qw48/d7UM1Uru"
-    assert extract_study_id_from_url(url) == "bve0Qw48"
+@pytest.mark.parametrize(
+    "input_url, expected_output",
+    [
+        ("https://lichess.org/study/RKEBYTWL/muR4Kgyc", "RKEBYTWL"), 
+        ("https://lichess.org/study/bve0Qw48/d7UM1Uru", "bve0Qw48"), 
+        ("https://lichess.org/study/RKEBYTWL", "RKEBYTWL")
+    ]
+)
+def test_extract_study_id_from_url_using_chapter(input_url, expected_output):
+    assert extract_study_id_from_url(input_url) == expected_output
 
 
 def test_get_study_chapters_count():
